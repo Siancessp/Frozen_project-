a7250c743c65a17f89c8fe0994eec01555155bc5
Siancessp <siancessp@gmail.com>
2024-07-13 13:30:16 +0530

    influencer

--- Ecomm/ecomApp/views.py
+++ Ecomm/ecomApp/views.py
@@ -736,3 +736,4 @@ class CouponList(generics.ListAPIView):
     def get_queryset(self):
         today = date.today()
         return CustomerCoupon.objects.filter(start_date__lte=today, expire_date__gte=today)
+
