014798c7547638581246524826fb8f81b392603f
Siancessp <163714161+Siancessp@users.noreply.github.com>
2024-08-07 20:16:58 +0530

    Update settings.py

--- Ecomm/Ecomm/settings.py
+++ Ecomm/Ecomm/settings.py
@@ -152,8 +152,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mithai01234@gmail.com'
-EMAIL_HOST_PASSWORD = 'ubpz dnre zoen rkuq'
+
 
 RAZORPAY_API_KEY="rzp_test_enEwAJBwuY35MP"
 RAZORPAY_SECRET_KEY="GDMhskdQyL9mC1OohkGJAoKC"
